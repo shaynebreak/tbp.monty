d92413fb25ef92564c4f07aa6ecbcbc773ca0828
AL <AL@RJSAL-ROB-OFC>
2025-04-14 11:00:29 -0400

    Bug fixes.
    -RA

--- src/tbp/monty/frameworks/models/rjs_al_ai_base.py
+++ src/tbp/monty/frameworks/models/rjs_al_ai_base.py
@@ -34,13 +34,17 @@ class ALHTMBase(MontyForGraphMatching):
         self.alhtm = self.gateway.entry_point
         self.alhtm.report("Initializing Python Hooks")
 
+    def pass_features_directly_to_motor_system(self, observation):
+        # do nothing so we don't step on our actions...
+
     def step(self, observations, *args, **kwargs):
         # Convert observations to Java-usable form
         # java_input = self._convert_obs(observations)
         # action = self.java_ai.getNextAction(java_input)
         # return self._convert_action(action)
         self.alhtm.report(str(observations))
-        return self.alhtm.getNextAction()
+        last_action = self.alhtm.getNextAction()
+        return last_action
 
     def _convert_obs(self, observations):
         # Implement this: convert Python dict -> Java structure
