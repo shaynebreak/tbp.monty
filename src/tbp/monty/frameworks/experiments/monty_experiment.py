9fdeb5bd25aeebfc7b04b3ff7a7ed4c0fc8cc1ef
AL <AL@RJSAL-ROB-OFC>
2025-04-14 12:50:48 -0400

    debug.
    -RA

--- src/tbp/monty/frameworks/experiments/monty_experiment.py
+++ src/tbp/monty/frameworks/experiments/monty_experiment.py
@@ -137,6 +137,8 @@ class MontyExperiment:
         motor_system_args = motor_system_config["motor_system_args"]
         assert issubclass(motor_system_class, MotorSystem)
         # motor_system = motor_system_class(rng=self.rng, **motor_system_args)
+        print("✅ Motor system selected {motor_system_class} args {motor_system_args}.")
+
         motor_system = ALHTMMotorSystem(rng=self.rng, **motor_system_args)
 
         # Get mapping between sensor modules, learning modules and agents
