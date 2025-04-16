326a2e95397fbc362a571b74461ae0f37f09a354
AL <AL@RJSAL-ROB-OFC>
2025-04-16 14:50:48 -0400

    Fixed ref.
    -RA

--- benchmarks/configs/my_experiments.py
+++ benchmarks/configs/my_experiments.py
@@ -9,13 +9,14 @@
 
 from dataclasses import asdict
 
-from benchmarks.configs.names import MyExperiments, YcbExperiments
+from benchmarks.configs.names import MyExperiments
+from benchmarks.configs.ycb_experiments import CONFIGS
 from tbp.monty.frameworks.config_utils.config_args import ALHTMMontyConfig
 import copy
 
 # Add your experiment configurations here
 # e.g.: my_experiment_config = dict(...)
-al_htm_obj_recog_experiment = copy.deepcopy(YcbExperiments["base_10simobj_surf_agent"])
+al_htm_obj_recog_experiment = copy.deepcopy(CONFIGS["base_10simobj_surf_agent"])
 al_htm_obj_recog_experiment.update(
     monty_config=ALHTMMontyConfig
 )
