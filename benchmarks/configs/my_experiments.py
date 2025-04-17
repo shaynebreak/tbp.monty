b2d8c5ebd463a1ba6f3b4e34bd93c12c05c845f6
AL <AL@RJSAL-ROB-OFC>
2025-04-17 10:30:28 -0400

    Reverted changes, will try another way to isolate rogue actions.
    -RA

--- benchmarks/configs/my_experiments.py
+++ benchmarks/configs/my_experiments.py
@@ -12,15 +12,13 @@ from dataclasses import asdict
 from benchmarks.configs.names import MyExperiments
 from benchmarks.configs.ycb_experiments import CONFIGS
 from tbp.monty.frameworks.config_utils.config_args import ALHTMMontyConfig
-from tbp.monty.frameworks.environments import embodied_data as ED
 import copy
 
 # Add your experiment configurations here
 # e.g.: my_experiment_config = dict(...)
 al_htm_obj_recog_experiment = copy.deepcopy(CONFIGS["base_10simobj_surf_agent"])
 al_htm_obj_recog_experiment.update(
-    monty_config=ALHTMMontyConfig(),
-    eval_dataloader_class=ED.EnvironmentDataLoaderPerObject
+    monty_config=ALHTMMontyConfig()
 )
 
 experiments = MyExperiments(
--- src/tbp/monty/frameworks/models/rjs_al_ai_base.py
+++ src/tbp/monty/frameworks/models/rjs_al_ai_base.py
@@ -51,12 +51,7 @@ class ALHTMMotorSystem(SurfacePolicyCurvatureInformed):
 
         self.action = None
         self.is_predefined = False  # required by base class
-        self.state = {
-            self.agent_id: {
-                "motor_only_step": False,
-                "sensors": {}  # You probably already have this populated later
-            }
-        }
+        self.state = {}  # this must be set externally
 
     def dynamic_call(self) -> Action:
         # TODO: wtf fix or remove if not needed:
