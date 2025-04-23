b14885ffd35383f0eb9f4cb1afd38786d6e79980
AL <AL@RJSAL-ROB-OFC>
2025-04-23 13:02:30 -0400

    Hookup fix with proper variable name?
    -RA

--- src/tbp/monty/frameworks/models/rjs_al_ai_base.py
+++ src/tbp/monty/frameworks/models/rjs_al_ai_base.py
@@ -131,7 +131,7 @@ class ALHTMMotorSystem(SurfacePolicyCurvatureInformed):
 
         global alhtm
 
-        alhtm = gateway.entry_point.getAlHtm(self.args.htm_config)
+        alhtm = gateway.entry_point.getAlHtm(kwargs.action_sampler_args.get("htm_config"))
         alhtm.report("Initializing Python ALHTMMotorSystem")
 
         self.action = None
